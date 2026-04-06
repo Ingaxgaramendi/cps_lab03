dfa892168c222b2bc77be558de81ee3664a8edcc
Gean Pierre <gean.ayala@ejemplo.com>
2026-04-05 22:11:14 -0500

    feat: actualizando todas las clases sprint-2

--- src/main/java/pe/edu/tecsup/lab01/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab01/controllers/StudentController.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab01.controllers;
 
 public class StudentController {
+    public void sprint2() {
+        System.out.println("Controller sprint-2");
+    }
 }
--- src/main/java/pe/edu/tecsup/lab01/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab01/repositories/StudentRepository.java
@@ -1,4 +1,8 @@
 package pe.edu.tecsup.lab01.repositories;
 
 public class StudentRepository {
+
+    public String Hola() {
+        return "hola";
+    }
 }
