aec39479f0ba918415949647c7270ee2eb62e9e1
Ingaxgaramendi <anderson.rivera.p@tecsup.edu.pe>
2026-04-05 22:14:33 -0500

    Nuevos metodos en student controller

--- src/main/java/pe/edu/tecsup/lab01/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab01/controllers/StudentController.java
@@ -1,4 +1,12 @@
 package pe.edu.tecsup.lab01.controllers;
 
 public class StudentController {
+
+    public String listStudents() {
+        return "Lista de estudiantes - sprint 2";
+    }
+
+    public String createStudent() {
+        return "Estudiante creado";
+    }
 }
